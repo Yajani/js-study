5acb95b1c633ab84bed7887eb9dd0ec179ecc6bd
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-15 11:47:07 +0900

    js 기초

--- hello.js
+++ hello.js
@@ -1,3 +1,11 @@
-const a = 10;
-const b = 30;
-console.log(a*b);
+
+var username;
+username = 'abc';
+
+console.log(username);
+
+// var my_home_address; //스네이크 케이스 X
+// var myHomeAddress;  //캐멀케이스 0
+
+var $$$$myHomeAddress___;
+
