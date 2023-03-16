26403913253fa4e192ae8aeb4c63c8a855ca3665
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-16 10:57:19 +0900

    무한루프

---
+++ 1. 자바스크립트 기초/12. 무한루프.js
@@ -0,0 +1,31 @@
+
+var n = 1;
+
+while(true){
+    console.log(n++);
+    if(n === 10) break;
+}
+
+
+for(;;){
+    console.log(n++);
+    if (n === 10) break;
+}
+
+console.log('안녕하세요');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
