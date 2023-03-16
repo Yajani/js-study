60163688a6366f8cb7ad50001b3817dba50fef73
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-16 15:12:29 +0900

    사칙연산 혼자 풀이

--- 1. 자바스크립트 기초/20. 사칙연산3.js
+++ 1. 자바스크립트 기초/20. 사칙연산3.js
@@ -1,39 +1,79 @@
 alert('~~~~~재미있는 사칙연산 게임 ~~~~~~\n[즐겁게 문제를 푸시다가 지겨우면0을 누르세요~]\n======================================');
 
-var right = 0, wrong = 0;
-while(true){
-    var front = Math.floor(Math.random() * 50) + 1
-    var back = Math.floor(Math.random() * 50) + 1
-    var n1 = front>back ? front:back;
-    var n2 = front>back ? back:front;
+while(true) {
+    var right = 0, wrong = 0;
+    var range = 0;
+    var count =1;
+    while(true){
+        alert('~~~~난이도를 설정합니다.~~~~~');
+        var level =  +prompt('[1. 상 (1~100) | 2. 중 (1~50) | 3. 하 (1~20)]');
+    
+        if(level === 1) {
+            range = 100;
+        } else if(level === 2){
+            range = 50;
+        } else if(level === 3) {
+            range = 20;
+        } else {
+            alert('난이도를 숫자로 다시 입력하세요 !');
+            continue;
+        }
+        break;
+    }
+    
+    //게임 종료 플래그
+    var gameEndFlag = false;
+    
+    while(true){
+    
+        var front = Math.floor(Math.random() * range) + 1
+        var back = Math.floor(Math.random() * range) + 1
+        var n1 = front>back ? front:back;
+        var n2 = front>back ? back:front;
+    
+        var option = Math.floor(Math.random() * 4) + 1
+        var result =0; 
+    
+    
+        var cal = '';
+        if(option === 1){
+            cal = '+';
+            result = n1 + n2;  
+        } else if(option === 2){
+            cal = '-';
+            result = n1 - n2;  
+        }
+        else if(option === 3){
+            cal = 'x';
+            result = n1 * n2;  
+        } else if(option === 4){
+            cal = '/';
+            result = n1 / n2;  
+        }
+    
+        var answer = +prompt(`Q${count}. ${n1} ${cal} ${n2} = ??`);
+        count++;
+    
+        if(answer === result){
+            alert('정답입니다 !');
+            right++; 
+        } else if(answer === 0) {
+            gameEndFlag = true;     
+            break;
+        }  
+        else {
+            alert('틀렸어요~');
+            wrong++;  
+        }         
+    }//end game loop
+                                                            
+    alert(`게임을 종료합니다!\n-------------------\n정답횟수 : ${right}회, 틀린횟수 : ${wrong}회`);
 
-    var op = Math.floor(Math.random() * 4) + 1
-    var result =0; 
-    var answer = '';
-    if(op === 1){
-        answer = +prompt(`Q${count}. ${n1} + ${n2} = ??`);
-        result = n1 + n2;  
-    } else if(op === 2){
-        var answer = +prompt(`Q${count}. ${n1} - ${n2} = ??`);
-        result = n1 - n2;  
+    if(gameEndFlag){
+        var exitFlag = confirm('한판 더 ?');
+        if(!exitFlag){ //(exitFlag === false)
+            alert('수고하셨습니다!');
+            break;
+        }
     }
-    else if(op === 3){
-        var answer = +prompt(`Q${count}. ${n1} * ${n2} = ??`);
-        result = n1 * n2;  
-    } else if(op === 4){
-        var answer = +prompt(`Q${count}. ${n1} / ${n2} = ??`);
-        result = n1 / n2;  
-    }
-
-    if(answer === result){
-        alert('정답입니다 !');
-        right++; 
-    } else if(answer !== result) {
-        alert('틀렸어요~');
-        wrong++;  
-    } else {
-        break;     
-    }
-}
-                                                        
-alert(`게임을 종료합니다!\n-------------------\n정답횟수 : ${right}회, 틀린횟수 : ${wrong}회`);
+}//end all game loop
