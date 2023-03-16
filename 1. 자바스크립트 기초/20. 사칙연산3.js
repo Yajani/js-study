feed548b5612536aed6901a86a24d3d81892558b
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-16 15:12:17 +0900

    사칙연산 혼자 풀이

--- 1. 자바스크립트 기초/20. 사칙연산3.js
+++ 1. 자바스크립트 기초/20. 사칙연산3.js
@@ -34,66 +34,6 @@ while(true){
     } else {
         break;     
     }
-
-    // switch(op){
-    //     case 1:
-    //         var answer = +prompt(`Q${count}. ${n1} + ${n2} = ??`);
-            
-    //         if(result === (front + back)){
-    //             alert('정답입니다 !');
-    //             right++;
-    //         }
-    //         else if(result === 0){
-    //             alert(`게임을 종료합니다!\n-------------------\n정답횟수 : ${right}회, 틀린횟수 : ${wrong}회`);
-    //             break;
-    //         } else {
-    //             alert('틀렸어요~');
-    //             wrong++;
-    //         }
-    //         break;
-    //         case 2:
-    //             var result = +prompt(`Q${count}. ${n1} - ${n2} = ??`);
-    //             if(result === (front > back? front - back: back - front)){
-    //                 alert('정답입니다 !');
-    //                 right++;
-    //             }else if(result === 0){
-    //                 alert(`게임을 종료합니다!\n-------------------\n정답횟수 : ${right}회, 틀린횟수 : ${wrong}회`);
-    //                 break;
-    //             }
-    //             else {
-    //                 alert('틀렸어요~');
-    //                 wrong++;
-    //             }
-    //             break;
-    //             case 3:
-    //                 var result = +prompt(`Q${count}. ${n1} x ${n2} = ??`);
-    //                 if(result === (front*back)){
-    //                     alert('정답입니다 !');
-    //                     right++;
-    //                 }else if(result === 0){
-    //                     alert(`게임을 종료합니다!\n-------------------\n정답횟수 : ${right}회, 틀린횟수 : ${wrong}회`);
-    //                     break;
-    //                 }else {
-    //                     alert('틀렸어요~');
-    //                     wrong++;
-    //         }
-    //         break;
-    //         case 4:
-    //             var result = +prompt(`Q${count}. ${n1} / ${n2} = ??`);
-    //             if(result === (front > back? front / back: back / front)){
-    //                 alert('정답입니다 !');
-    //                 right++;
-    //             }else if(result === 0){
-    //                 alert(`게임을 종료합니다!\n-------------------\n정답횟수 : ${right}회, 틀린횟수 : ${wrong}회`);
-    //                 break;
-    //             }
-    //             else {
-    //                 alert('틀렸어요~');
-    //                 wrong++;
-    //             }
-    //             break;
-    // }
-            
 }
                                                         
 alert(`게임을 종료합니다!\n-------------------\n정답횟수 : ${right}회, 틀린횟수 : ${wrong}회`);
