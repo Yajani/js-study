ca9ff3130375522c4850da771d04f1a29f9cb63f
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-17 10:50:29 +0900

    배열기초

---
+++ 2. 배열/1. 배열기초.js
@@ -0,0 +1,45 @@
+
+var a = 10;
+var b = a;
+a=15;
+console.log(b);
+
+var arr1 = [1,2,3,4];
+var arr2 = arr1;
+
+arr1[1] = 999;
+
+arr2[0] = 77;
+
+
+console.log('arr1:',arr1);
+console.log('arr2:',arr2);
+
+console.log('========================');
+
+//배열의 생성 
+//배열 변수 이름 관례 : 복수형, list어미 
+var fruits = [
+    '오렌지',
+    '라임',
+    '레몬'
+]; //배열 리터럴
+
+//유사배열
+var arrayLike = {
+    0: '자몽',
+    1: '포도',
+    2: '한라봉',
+    length: 3,
+};
+
+console.log(typeof fruits);
+console.log(fruits);
+console.log(arrayLike);
+
+console.log(`배열의 데이터 수: ${fruits.length}`);
+
+
+
+
+
