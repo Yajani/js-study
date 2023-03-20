44e53387cbec505608800a391adc87c1cd839f12
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-20 10:26:14 +0900

    객체 추가,삭제, 수정 반복문

--- 3. 객체/1. 객체 기초.js
+++ 3. 객체/1. 객체 기초.js
@@ -43,6 +43,61 @@ console.log(dog);
 dog.favorite.slice(1).splice(1, 1);
 
 
+// 프로퍼티 참조2
+console.log('=================================');
+console.log(dog.injection);
+console.log(dog['injection']);
+
+//key를 변수에 저장 
+var fv = 'favorite';
+console.log(cat[fv]);
+// console.log(cat['fv']); //변수에 담아서 참조에는 ''을 사용하지 않는다
+
+//프로퍼티 값 수정 
+dog.age = 4;
+cat.favorite[1] = '실뭉치';
+
+dog['injection'] = false;
+console.log(dog);
+
+//프로퍼티 동적 추가 
+cat.owner = '김철수'; //추가
+cat.owner = '박영희'; //수정
+console.log(cat);
+
+//프로퍼티 삭제 
+delete cat.owner;
+console.log(cat);
+
+//프로퍼티 존재 유무 확인 
+//key를 반드시 문자열로 표기해야함.
+
+var ageFlag = 'age123' in cat;
+console.log(ageFlag);
+
+// master라는 프로퍼티가 없으면 추가해라 
+if(!('master' in cat)){
+    cat['master'] = '김또또';
+}
+console.log(cat);
+
+console.log('============================');
+//객체를 순회하는 반복문
+//key를 반복해서 추출해줌 
+for(var k in dog) {
+    // console.log(k);
+    // value를 추출하는 방법
+    console.log(dog[k]);
+}
+
+
+
+
+
+
+
+
+
 
 
 
