90d7d23d84e9ba2e5a39159cd63be0980752583d
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-21 10:38:57 +0900

    const

---
+++ 4. 함수/11.const.js
@@ -0,0 +1,35 @@
+const TODAY_SALE_RATE = 0.2;
+
+const PI = 3.14159265;
+
+// PI =5.5; //상수는 수정불가
+console.log(PI);
+
+//객체는 불변성을 유지하는게 좋다 
+const kim = {
+    name: '김철수',
+    age : 25
+};
+
+kim.name = '김칠리';
+
+// kim = {
+//     name: '김철수',
+//     age : 25
+// };
+
+
+const a = 10;
+const b = 20;
+
+for(let i =0; i < 5; i++) {
+    
+}
+
+
+
+
+
+
+
+
